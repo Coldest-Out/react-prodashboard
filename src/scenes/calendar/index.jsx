131321419481a7e9b5d97e4dd0ddf6a8e9977621
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-04-20 17:42:36 -0700

    added sidebar for calendar component

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -45,5 +45,48 @@ const Calendar = () => {
       selected.event.remove();
     }
   };
-}
+
+	return 
+	<Box m="20px">
+		<Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+
+		<Box display="flex" justifyContent="space-between">
+			{/** CALENDAR SIDE BAR */}
+			<Box 
+				flex="1 1 20%" 
+				backgroundColor={colors.primary[400]} 
+				p="15px" 
+				borderRadius="4px"
+				>
+					<Typography variant="h5">Events</Typography>
+					<List>
+						{currentEvents.map((event) => (
+							<ListItem
+							key={event.id}
+							sx={{ 
+								backgroundColor: colors.greenAccent[500], 
+								margin: "10px 0", 
+								borderRadius: "2px", 
+							}}
+						>
+							<ListItemText
+								primary={event.title}
+								secondary={
+									<Typography>
+										{formatDate(event.start, {
+											year: "numeric",
+											month: "short",
+											day: "numeric",
+										})}
+									</Typography>
+								}
+							/>
+						</ListItem>
+						))}
+					</List>
+			</Box>
+		</Box>
+	</Box>
+};
+
 export default Calendar;
