b97e17d4ece8d0a28a37f4c963837915d9b92cab
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-01 17:11:40 -0700

    created faq scene/page

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
 import Calendar from './scenes/calendar';
 import Form from './scenes/form';
+import FAQ from './scenes/faq';
 // import Bar from './scenes/global/bar';
 // import Line from './scenes/global/line';
 // import Pie from './scenes/global/pie';
-// import FAQ from './scenes/global/faq';
 // import Geography from './scenes/global/geography';
 
 
@@ -35,10 +35,10 @@ function App() {
                 <Route path="/invoices" element={<Invoices />}/>
                 <Route path="/form" element={<Form />}/>
                 <Route path="/calendar" element={<Calendar />}/>
+                <Route path="/faq" element={<FAQ />}/>
                 {/* <Route path="/bar" element={<Bar />}/> */}
                 {/* <Route path="/pie" element={<Pie />}/> */}
                 {/* <Route path="/line" element={<Line />}/> */}
-                {/* <Route path="/faq" element={<FAQ />}/> */}
                 {/* <Route path="/geography" element={<Geography />}/> */}
                 
               </Routes>
