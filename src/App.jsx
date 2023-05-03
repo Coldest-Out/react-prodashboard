f02aed2d69a1cfd4d9a7f9aa20b298f7a6c334c6
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-05-03 14:48:23 -0700

    added geography chart component and scene

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ import Calendar from './scenes/calendar';
 import Form from './scenes/form';
 import FAQ from './scenes/faq';
 import Bar from './scenes/bar';
-// import Line from './scenes/global/line';
+import Line from './scenes/line';
 import Pie from './scenes/pie';
-// import Geography from './scenes/global/geography';
+import Geography from './scenes/geography';
 
 
 function App() {
@@ -38,8 +38,8 @@ function App() {
                 <Route path="/faq" element={<FAQ />}/>
                 <Route path="/bar" element={<Bar />}/>
                 <Route path="/pie" element={<Pie />}/>
-                {/* <Route path="/line" element={<Line />}/> */}
-                {/* <Route path="/geography" element={<Geography />}/> */}
+                <Route path="/line" element={<Line />}/>
+                <Route path="/geography" element={<Geography />}/>
                 
               </Routes>
               
